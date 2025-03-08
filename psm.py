1d5a3c09636979a5a4926e0ef0b6864a017e519d
AlishaAmber <161523298+AlishaAmber@users.noreply.github.com>
2025-03-08 23:16:25 +0500

    psm.py

--- psm.py
+++ psm.py
@@ -8,7 +8,7 @@ st.markdown("""
 <style>
     .main {text-align: center;}
     .stTextInput {width; 60% !important; margin: auto;}
-    .stButton button {width: 50%; background-color yellow; color: white; font-size: 18px; }
+    .stButton button {width: 50%; background-color black; color: white; font-size: 18px; }
     .stButton button:hover { background-color: red; color:white}
 </style>
 """, unsafe_allow_html=True)
